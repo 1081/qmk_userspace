07a8226924eb9ef712505bb6d2c3d45aea32f5f0
Jan Sattler <jan1081@pm.me>
2024-05-26 00:25:47 +0200

    test2

--- keyboards/keycapsss/3w6_2040/keymaps/jan1081/keymap.c
+++ keyboards/keycapsss/3w6_2040/keymaps/jan1081/keymap.c
@@ -248,7 +248,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
     }
     return true;
-    
 }
 
 // --- RGB LIGHTING ------------------------------
